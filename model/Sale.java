c2ed42ed77c40bc97d8bab7a64b78fb8fa3e1220
slyngen <1089437@ucn.dk>
2021-05-25 15:36:06 +0200

    Made Sale class

---
+++ model/Sale.java
@@ -0,0 +1,38 @@
+package model;
+import java.util.Date;
+import java.util.ArrayList;
+
+
+/**
+ * Write a description of class Sale here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Sale
+{
+    // instance variables - replace the example below with your own
+    private String store;
+    private Date dateTime;
+    private double discount;
+    private String state;
+    private ArrayList<SalesLineItem> saleLines;
+    private Customer customer;
+    private Employee employee;
+
+    /**
+     * Constructor for objects of class Sale
+     */
+    public Sale(Employee employee)
+    {
+        // initialise instance variables
+        this.saleLines = new ArrayList<>();
+        this.employee = employee;
+        dateTime = new Date();
+    }
+    
+    private void addSalesLineItem(SalesLineItem salesLineItem)
+    {
+        saleLines.add(salesLineItem);
+    }
+}
