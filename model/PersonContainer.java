fa75fd06caf8c749c063aeefa2019a60cd0a420f
IanStroemkjaerJensen <1089449@ucn.dk>
2021-05-25 15:08:37 +0200

    Implemented PersonContainer class

---
+++ model/PersonContainer.java
@@ -0,0 +1,44 @@
+package model;
+import java.util.HashMap;
+
+
+/**
+ * Write a description of class PersonContainer here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class PersonContainer
+{
+    // instance variables - replace the example below with your own
+    private static PersonContainer instance;
+    private HashMap<String, Customer> customerContainer;
+    private HashMap<String, Employee> employeeContainer;
+
+    /**
+     * Constructor for objects of class PersonContainer
+     */
+    private PersonContainer()
+    {
+        // initialise instance variables
+        customerContainer = new HashMap<>();
+        employeeContainer = new HashMap<>();
+    }
+    
+    public static PersonContainer getInstance()
+    {
+        // Singleton
+        if(instance == null) {
+            instance = new PersonContainer();
+        }
+        return instance;
+    }
+    
+    public Customer findCustomerByCustomerID(String customerID)
+    {
+        // Returns customer value in HashMap stored by key
+        return customerContainer.get(customerID);
+    }
+    
+    
+}
