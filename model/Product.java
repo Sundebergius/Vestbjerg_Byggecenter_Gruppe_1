d483dcb2daad9d846cd91d2f544a530d5e0a0d5f
Andreas Clement Holmgaard <1089448@ucn.dk>
2021-05-27 10:00:34 +0200

    Added getters to product

--- model/Product.java
+++ model/Product.java
@@ -17,18 +17,31 @@ public abstract class Product
     /**
      * Constructor for objects of class Product
      */
-    public Product(String name, String description, double price, String barcode){
+    public Product(String name, String description, double price, String barcode)
+    {
         this.description = description;
         this.price = price;
         this.barcode = barcode;
         this.name = name;
     }
     
-    public String getBarcode(){
+    public String getBarcode()
+    {
         return barcode;
     }
     
-    public double getPrice(){
+    public double getPrice()
+    {
         return price;
+    }
+    
+    public String getName()
+    {
+        return name;   
+    }
+    
+    public String getDescription()
+    {
+        return description;
     }
 }
